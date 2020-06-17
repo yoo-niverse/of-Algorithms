f03a8fe3d7c2b2c4cb63cee7172630554420e5c7
YOONSXNG <yoonsungkr@kakao.com>
2020-06-18 01:04:40 +0900

    Studied on June 18th

--- 003. Binary Search Algorithm [이진탐색 구현]/binary search.py
+++ 003. Binary Search Algorithm [이진탐색 구현]/binary search.py
@@ -1,6 +1,18 @@
-lista = [1,2,3,4,5]
-listb = [6,8,9]
+def binary_search(element, some_list):
+    
+    while True:
+        mid = (len(some_list)-1) // 2
+        if some_list[mid] == element:
+            return mid
+        elif some_list[mid] > element:
+            some_list = some_list[0:(mid-1)]
+        elif some_list[mid] < element:
+            some_list = some_list[(mid+1):(len(some_list)-1)]
 
-lista = listb
 
-print(lista)
+
+print(binary_search(2, [2, 3, 5, 7, 11]))
+print(binary_search(0, [2, 3, 5, 7, 11]))
+print(binary_search(5, [2, 3, 5, 7, 11]))
+print(binary_search(3, [2, 3, 5, 7, 11]))
+print(binary_search(11, [2, 3, 5, 7, 11]))
