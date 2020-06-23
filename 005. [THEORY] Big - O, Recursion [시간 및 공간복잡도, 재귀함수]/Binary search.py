64cb62819866a0f7893bf2d50321b0f578bfbb6d
YOONSXNG <yoonsungkr@kakao.com>
2020-06-23 23:54:53 +0900

    Studied on June 23th
    재귀함수를 이용하여 이진탐색 구현하기

--- 005. [THEORY] Big - O, Recursion [시간 및 공간복잡도, 재귀함수]/Binary search.py
+++ 005. [THEORY] Big - O, Recursion [시간 및 공간복잡도, 재귀함수]/Binary search.py
@@ -3,18 +3,25 @@ def binary_search(element, some_list, start_index=0, end_index=None):
     # end_index가 따로 주어지지 않은 경우에는 리스트의 마지막 인덱스
     if end_index == None:
         end_index = len(some_list) - 1
-    mid = start_index + end_index
+    mid = start_index + end_index // 2
 
     if some_list[mid] == element:
         return mid
-    elif some_list[mid] > element:
-        end_index = mid -1
-        start_index = 0
-        binary_search(element, some_list, start_index, end_index)
-    elif some_list[mid] < element:
-        start_index = mid+1
-        end_index = None
-        binary_search(element, some_list, start_index, end_index)
+        # 아래. 생각치 못한 부분 / 작성한 내용이 잘못되었음
+    # elif some_list[start_index] > element or some_list[end_index] < element:
+    if start_index > end_index:
+        return None
+    '''elif start_index == end_index:
+        if some_list[start_index] == element:
+            return start_index'''
+
+    if some_list[mid] > element:
+        return binary_search(element, some_list, 0, mid-1)
+
+    if some_list[mid] < element:
+        return binary_search(element, some_list, mid+1)
+
+    # return을 안써줘서 다 None 나옴. 반환을 안해주니까 넌이지 없는 넌이 아니고
 
 ''' 
 BASE CASE : 중간값(mid)가 element와 같을 때
