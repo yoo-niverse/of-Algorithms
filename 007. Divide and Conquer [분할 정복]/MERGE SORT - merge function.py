e795d2427370701466f91986b492b1c3a83d2ace
YOONSXNG <yoonsungkr@kakao.com>
2020-06-30 02:12:43 +0900

    Update MERGE SORT - merge function.py

--- 007. Divide and Conquer [분할 정복]/MERGE SORT - merge function.py
+++ 007. Divide and Conquer [분할 정복]/MERGE SORT - merge function.py
@@ -21,6 +21,7 @@ def merge(list1, list2):
 print(merge([1],[]))
 print(merge([],[1]))
 print(merge([2],[1]))
+print(merge([2],[2]))
 print(merge([1, 2, 3, 4],[5, 6, 7, 8]))
 print(merge([5, 6, 7, 8],[1, 2, 3, 4]))
 print(merge([4, 7, 8, 9],[1, 3, 6, 10]))
