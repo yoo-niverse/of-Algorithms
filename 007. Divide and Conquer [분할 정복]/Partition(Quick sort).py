b834cc481a15b4078029cc7c662e23487af7e242
YOONSXNG <yoonsungkr@kakao.com>
2020-07-05 02:09:12 +0900

    Studied on July 5th

---
+++ 007. Divide and Conquer [분할 정복]/Partition(Quick sort).py
@@ -0,0 +1,40 @@
+# 두 요소의 위치를 바꿔주는 helper function
+def swap_elements(my_list, index1, index2):
+    tmp = my_list[index1]
+    my_list[index1] = my_list[index2]
+    my_list[index2] = tmp
+
+# 퀵 정렬에서 사용되는 partition 함수
+def partition(my_list, start, end):
+    # 코드를 작성하세요.
+    i = b = start
+    p = end
+
+    if len(my_list) == 3 and my_list[p] > my_list[p-1] and my_list[p] < my_list[p+1]:
+        return my_list
+  # BASE CASE를 어떻게 코드로 표현할까?
+    while i < p:
+        if my_list[i] > p:
+            i += 1
+        else :
+            swap_elements(my_list, b, i)
+            b += 1
+            i += 1
+
+    swap_elements(my_list, b, p)
+    partition(my_list, 0, b-1)
+    partition(my_list, b+1, i)
+    return p
+
+
+# 테스트 1
+list1 = [4, 3, 6, 2, 7, 1, 5]
+pivot_index1 = partition(list1, 0, len(list1) - 1)
+print(list1)
+print(pivot_index1)
+
+# 테스트 2
+list2 = [6, 1, 2, 6, 3, 5, 4]
+pivot_index2 = partition(list2, 0, len(list2) - 1)
+print(list2)
+print(pivot_index2)
