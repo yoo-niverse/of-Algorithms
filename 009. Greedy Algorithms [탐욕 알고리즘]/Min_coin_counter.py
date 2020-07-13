9fef32265dd20bac25c3ee7d3fa526a4a9d6086e
YOONSXNG <yoonsungkr@kakao.com>
2020-07-14 02:09:16 +0900

    Greedy Algorithm

--- 009. Greedy Algorithms [탐욕 알고리즘]/Min_coin_counter.py
+++ 009. Greedy Algorithms [탐욕 알고리즘]/Min_coin_counter.py
@@ -3,20 +3,25 @@ def min_coin_count(value, coin_list):
     amount_coin = []
     amount_coin.append((value//500))
     amount_coin.append((value-(amount_coin[0]*500))//100)
-    amount_coin.append((value-(amount_coin[1]*100))//50)
-    amount_coin.append((value-(amount_coin[2]*50))//10)
+    amount_coin.append((value-(amount_coin[0]*500+amount_coin[1]*100))//50)
+    amount_coin.append((value-(amount_coin[0]*500+amount_coin[1]*100+amount_coin[2]*50))//10)
+        # 금액별 동전으로 계산하여 리스트에 동전의 수를 저장
+
 
     total = 0
-    for i in range(len(amount_coin)-1):
-        print(amount_coin[i])
+    for i in range(len(amount_coin)):
+        total += amount_coin[i]
+            # 리스트에 저장된 동전의 수를 모두 더한다.
+            ''' sorted를 통해 coin_list를 정렬하고, for문을 통해 리스트의 요소를 불러온 뒤 즉시 나누어 가산할 수 있다.
+                거슬러줄 금액이 변하는 것에 대해서는 % 연산을 통해 즉각 업데이트 해준다. '''
 
     return total
 # 테스트
 default_coin_list = [100, 500, 10, 50]
 print(min_coin_count(1440, default_coin_list))
-#print(min_coin_count(1700, default_coin_list))
-#print(min_coin_count(23520, default_coin_list))
-#print(min_coin_count(32590, default_coin_list))
+print(min_coin_count(1700, default_coin_list))
+print(min_coin_count(23520, default_coin_list))
+print(min_coin_count(32590, default_coin_list))
 
 '''
 1. 최적 부분구조가 존재하는가?
