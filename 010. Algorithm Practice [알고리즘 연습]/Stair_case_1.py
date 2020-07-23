5f48cbbe91829cedd622655130424999cadd1547
YOONSXNG <yoonsungkr@kakao.com>
2020-07-24 00:46:02 +0900

    Update Stair_case_1.py

--- 010. Algorithm Practice [알고리즘 연습]/Stair_case_1.py
+++ 010. Algorithm Practice [알고리즘 연습]/Stair_case_1.py
@@ -3,11 +3,34 @@ def staircase(n):
     if n <= 1:
         return 1
 
-    ONLY_1STAIR = 1
-        # 계단을 1칸씩만 올라가서 도달하는 경우의 수는 항상 1개
+    case_count = []
+    case_count.append(1)         # 계단을 1칸씩만 올라가서 도달하는 경우의 수는 항상 1개
+    case_count.append(n-1)       # 딱 한번만 2칸을 올라가서 도달하는 경우의 수는 n-1개
+    left_stairs = n
 
-    once_2stairs = n-1
-        # 딱 한번만 2칸을 올라가서 도달하는 경우의 수는 n-1개
+    count_two_more = n // 2
+
+    if count_two >= 2:
+        arrange = n-2
+        case_count.append(arrange * (arrange-1) // 2)
+
+    for i in range(2, count_two_more+1):
+        arrange = n-i
+        # 순열 공식을 어떻게 구현하지?
+
+    return sum(case_count)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
