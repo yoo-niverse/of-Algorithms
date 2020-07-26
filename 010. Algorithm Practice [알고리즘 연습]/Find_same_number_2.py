3fe893f292209e0cd9550e221dbb1ae34b0c0deb
YOONSXNG <yoonsungkr@kakao.com>
2020-07-26 23:36:45 +0900

    Start Find_same_number_2

---
+++ 010. Algorithm Practice [알고리즘 연습]/Find_same_number_2.py
@@ -0,0 +1,9 @@
+def find_same_number(some_list, start, end):
+    # 필요한 경우, start와 end를 옵셔널 파라미터로 만들어도 됩니다.
+    # 코드를 쓰세요
+
+
+# 중복되는 수 ‘하나’만 리턴합니다.
+print(find_same_number([1, 4, 3, 5, 3, 2]))
+print(find_same_number([4, 1, 5, 2, 3, 5]))
+print(find_same_number([5, 2, 3, 4, 1, 6, 7, 8, 9, 3]))
